4361f674092277c4b8f9703a0aa54674dd814df2
frd <rundafang@gmail.com>
2025-11-27 19:46:34 +0800

    QuadcopterEnv大删，删除障碍物、相机相关内容。修正部分错误

--- rsl_rl/rsl_rl/modules/actor_critic.py
+++ rsl_rl/rsl_rl/modules/actor_critic.py
@@ -44,6 +44,7 @@ class ActorCritic(nn.Module):
         for layer_index in range(len(actor_hidden_dims)):
             if layer_index == len(actor_hidden_dims) - 1:
                 actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], num_actions))
+                actor_layers.append(nn.Tanh())  # Add tanh activation to output layer
             else:
                 actor_layers.append(nn.Linear(actor_hidden_dims[layer_index], actor_hidden_dims[layer_index + 1]))
                 actor_layers.append(activation)
